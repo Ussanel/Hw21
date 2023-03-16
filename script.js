cf8a3e80b0d2c23946110f76b93e95847a20da38
Verezub Olena <ussanel@ukr.net>
2023-03-16 22:34:22 +0200

    ad all files, done 1*3 ex

---
+++ script.js
@@ -0,0 +1,43 @@
+// Завдання 1
+// const car={
+//     model: 'audi',
+//     year: 2000,
+//     color: 'white'
+// }
+// let key = prompt('What color do you want?', 'color')
+// car.color = key;
+// car.type = 'electro'
+// alert(car.color)
+// console.log (car)
+
+// Завдання 2
+// salaries.total()
+// salaries.manager = 5000
+// salaries.total()
+
+// let salaries = {
+//     fronted: 12000,
+//     backend: 10000,
+//     designer: 8000,
+//     dayPay: function () {
+//         alert('We must pay salary on Tuesday!');
+//     },
+// };
+// total(){
+// let sum = this.fronted + this.backend + this.designer;
+//     console.log(sum)
+// }
+
+
+
+// // Завдання 3
+// function Lego(brand, system, cost) {
+//     this.brand = brand,
+//         this.system = system,
+//         this.cost = cost
+// };
+// let dell = new Lego('Dell', 'windows', 800)
+// console.log (dell)
+// let apple = new Lego('Apple', 'MAC OS', 1700)
+// console.log(apple)
+// Завдання 4
